e3a961af36135addf933f289126d01abcfba0d23
NULL <rgtntppy@gmail.com>
2024-10-25 12:28:27 +0900

    welcomeImgコンポーネント(トップanimation)作成・追加

---
+++ src/Components/main/Contents/WelcomeImg/WelcomeImg.tsx
@@ -0,0 +1,40 @@
+import 'Components/main/Contents/WelcomeImg/welcomeImg.scss';
+import { useState, useEffect } from 'react';
+
+const WelcomeImg: React.FC = () => {
+    const [welcomeText, setWelcomeText] = useState<string>('');
+    const [animate, setAnimate] = useState<boolean>(false);
+
+    useEffect(() => {
+        const timeoutId1 = setTimeout(() => {
+            setWelcomeText('welcome!');
+        
+            const timeoutId2 = setTimeout(() => {
+                setAnimate(true);
+                setTimeout(() => {
+                    setWelcomeText('MyIntroduction');
+                }, 3000);
+            }, 6000);
+            
+            return () => clearTimeout(timeoutId2);
+        }, 1000);
+
+        return () => clearTimeout(timeoutId1);
+    }, []);
+
+    return (
+        <div className='welcomeImg'>
+            {welcomeText === 'welcome!' ? (
+                <p className={`welcomeText ${animate ? 'slideOut' : ''}`}>
+                    {welcomeText}
+                </p>
+            ) : (
+                <p className='welcomeTextChange'>
+                    {welcomeText}
+                </p>
+            )}
+        </div>
+    );
+};
+
+export default WelcomeImg;
---
+++ src/Components/main/Contents/WelcomeImg/welcomeImgInterface.ts
@@ -0,0 +1,3 @@
+export interface WelcomeImgInterface {
+    welcomeText:string;
+}
