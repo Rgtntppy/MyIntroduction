bdc43833cc0c107f9090ba7b2eca3e611fbaf3cf
NULL <rgtntppy@gmail.com>
2024-10-25 12:27:31 +0900

    mainコンポーネント作成・追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import 'App.css';
 import Header from 'Components/Header/Header';
+import Main from 'Components/main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Components/main/Main.tsx
@@ -0,0 +1,9 @@
+import WelcomeImg from 'Components/main/Contents/WelcomeImg/WelcomeImg';
+
+const Main = () => {
+    return (
+        <WelcomeImg/>
+    );
+}
+
+export default Main;
